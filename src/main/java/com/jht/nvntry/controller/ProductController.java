e876d67862dfd3d6af555477607cd9224fadc3f2
J-H-Tran <tr.h.jonathan@gmail.com>
2026-04-08 05:01:08 +0900

    added check availability endpoint

--- src/main/java/com/jht/nvntry/controller/ProductController.java
+++ src/main/java/com/jht/nvntry/controller/ProductController.java
@@ -1,10 +1,11 @@
 package com.jht.nvntry.controller;
 
+import com.jht.nvntry.domain.inventory.dto.InventoryAvailabilityDTO;
 import com.jht.nvntry.domain.product.dto.ProductCreationDTO;
 import com.jht.nvntry.domain.product.dto.ProductDTO;
 import com.jht.nvntry.domain.product.dto.ProductModificationDTO;
+import com.jht.nvntry.service.InventoryService;
 import com.jht.nvntry.service.ProductService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
@@ -22,11 +24,14 @@ import java.util.UUID;
 @RequestMapping("products")
 public class ProductController {
     private final ProductService productService;
+    private final InventoryService inventoryService;
 
     public ProductController(
-            ProductService productService
+            ProductService productService,
+            InventoryService inventoryService
     ) {
         this.productService = productService;
+        this.inventoryService = inventoryService;
     }
 
     @GetMapping
@@ -62,5 +67,11 @@ public class ProductController {
     }
 
     // check availability
-    //@GetMapping("{id}/inventory?")
+    @GetMapping("{id}/inventory")
+    public ResponseEntity<InventoryAvailabilityDTO> checkAvailability(
+            @PathVariable("id") UUID prodId,
+            @RequestParam("location") UUID locId
+    ) {
+        return ResponseEntity.ok(inventoryService.checkAvailability(prodId, locId));
+    }
 }
