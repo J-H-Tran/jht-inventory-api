1f85b166fd510fb3fec24efce908d5e9d7ae1891
J-H-Tran <tr.h.jonathan@gmail.com>
2026-04-08 05:01:35 +0900

    added repositories for check availability endpoint

---
+++ src/main/java/com/jht/nvntry/repository/InventoryItemRepository.java
@@ -0,0 +1,12 @@
+package com.jht.nvntry.repository;
+
+import com.jht.nvntry.domain.inventory.InventoryItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, UUID> {
+    Optional<InventoryItem> findByProductIdAndLocationId(UUID prodId, UUID locId);
+}
