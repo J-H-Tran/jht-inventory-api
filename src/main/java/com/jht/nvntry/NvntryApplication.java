a57a32d9c39029a5c96bdba8e173a1a055b5faab
J-H-Tran <tr.h.jonathan@gmail.com>
2026-04-07 22:10:43 +0900

    initial commit

---
+++ src/main/java/com/jht/nvntry/NvntryApplication.java
@@ -0,0 +1,13 @@
+package com.jht.nvntry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvntryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jht/nvntry/NvntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jht.nvntry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
