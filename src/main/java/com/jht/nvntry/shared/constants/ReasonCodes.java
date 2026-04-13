a65a5d120ed9608992f3ccb8cd860972fef68477
J-H-Tran <tr.h.jonathan@gmail.com>
2026-04-14 00:39:49 +0900

    added reason codes class, too early to use

---
+++ src/main/java/com/jht/nvntry/shared/constants/ReasonCodes.java
@@ -0,0 +1,13 @@
+package com.jht.nvntry.shared.constants;
+
+public final class ReasonCodes {
+    public static final String INITIAL_STOCK = "INITIAL_STOCK";
+    public static final String DAMAGE = "DAMAGE";
+    public static final String THEFT = "THEFT";
+    public static final String AUDIT_CORRECTION = "AUDIT_CORRECTION";
+    public static final String RETURN_TO_SUPPLIER = "RETURN_TO_SUPPLIER";
+    public static final String FOUND_STOCK = "FOUND_STOCK";
+    public static final String SYSTEM_CORRECTION = "SYSTEM_CORRECTION";
+
+    private ReasonCodes() {}
+}
